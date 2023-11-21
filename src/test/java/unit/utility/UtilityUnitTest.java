c6a909649281bebdec8a82415930193a0e250036
SimplyMarious <mariothemaster98@hotmail.it>
2023-11-21 17:53:03 +0100

    Remove comment on UtilityUnitTest

--- src/test/java/unit/utility/UtilityUnitTest.java
+++ src/test/java/unit/utility/UtilityUnitTest.java
@@ -34,48 +34,15 @@ public class UtilityUnitTest {
 
     @Test
     public void testGetValueFromPropertiesWithRightKeyAndValue() throws IOException {
-        // Arrange
         String key = "testKey";
         String expectedValue = "testValue";
 
-        // Stubbing: When properties.load() is called, return null (you can change this based on your specific needs)
         doNothing().when(mockProperties).load(mockFileInputStream);
-
-        // Stubbing: When properties.getProperty(key) is called, return the expected value
         when(mockProperties.getProperty(key)).thenReturn(expectedValue);
 
-        // Assert
         assertThat(Utility.getValueFromProperties(key), is(expectedValue));
 
-        // Verify that properties.load() is called once with the provided FileInputStream
         verify(mockProperties, times(1)).load(mockFileInputStream);
-
-        // Verify that properties.getProperty(key) is called once with the provided key
         verify(mockProperties, times(1)).getProperty(key);
     }
-
-//    @Test
-//    public void testGetValueFromPropertiesWithError() throws IOException {
-//        // Arrange
-//        String key = "testKey";
-//
-//        // Stubbing: When properties.load() is called, throw an IOException
-//        doThrow(new IOException("Test IOException")).when(mockProperties).load(mockFileInputStream);
-//
-//        // Act and Assert
-//
-//        Utility.getValueFromProperties(key);
-//        // If no exception is thrown, fail the test
-//        fail("Expected IOException was not thrown");
-//
-//            // Verify that properties.load() is called once with the provided FileInputStream
-//            verify(mockProperties, times(1)).load(mockFileInputStream);
-//
-//            // Verify that the error message is printed to System.err
-//            // Note: You may need to adjust this verification based on your specific error handling logic
-//            assertThat(e.getMessage(), is("Error in reading properties file."));
-//        }
-//    }
-
-
 }
