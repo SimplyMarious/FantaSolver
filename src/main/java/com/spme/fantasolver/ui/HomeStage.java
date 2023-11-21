f6fa06fab70836c36c54b91de8b229eef4531540
SimplyMarious <mariothemaster98@hotmail.it>
2023-11-21 21:21:59 +0100

    Add buttonAddTeam to HomeStage

--- src/main/java/com/spme/fantasolver/ui/HomeStage.java
+++ src/main/java/com/spme/fantasolver/ui/HomeStage.java
@@ -2,13 +2,19 @@ package com.spme.fantasolver.ui;
 
 import com.spme.fantasolver.Application;
 import com.spme.fantasolver.controllers.HomeController;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 
 public class HomeStage {
     private final HomeController homeController;
+
+    private Button buttonAddTeam;
+
     public HomeStage(boolean doesTeamExist) {
         this.homeController = HomeController.getInstance();
         homeController.handleInitialization(this, doesTeamExist);
@@ -17,6 +23,14 @@ public class HomeStage {
     public void initializeStage() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("home-stage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+
+        buttonAddTeam = (Button)fxmlLoader.getNamespace().get("buttonAddTeam");
+        buttonAddTeam.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                buttonAddTeam.setVisible(false);
+            }
+        });
 
         Application.getStage().setTitle("FantaSolver - Home");
         Application.getStage().setScene(scene);
