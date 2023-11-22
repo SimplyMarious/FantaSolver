cc33459a5130648076be11d5c94110660086aa08
rbgdotbrush <rbg94@hotmail.it>
2023-11-22 20:28:14 +0100

    Implement signUp and signIn methods in UserDAOMySQL

--- src/main/java/com/spme/fantasolver/dao/UserDAOMySQL.java
+++ src/main/java/com/spme/fantasolver/dao/UserDAOMySQL.java
@@ -1,15 +1,51 @@
 package com.spme.fantasolver.dao;
 
+import java.sql.*;
+
 public class UserDAOMySQL implements UserDAO{
     @Override
-    // TODO: create a new user
     public boolean signUp(String username, String password) {
+        Connection connection = DataRetriever.connectToDatabase();
+
+        try {
+            String insertQuery = "INSERT INTO user (name, password) VALUES (?, ?)";
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, password);
+
+                preparedStatement.executeUpdate();
+            }
+
+            return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return false;
     }
 
     @Override
-    // TODO: retrieve a user if it exists
     public boolean signIn(String username, String password) {
+        Connection connection = DataRetriever.connectToDatabase();
+
+        try {
+            String insertQuery = "SELECT * FROM user WHERE name = ? AND password = ?";
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, password);
+
+                ResultSet result = preparedStatement.executeQuery();
+
+                if(result.next())
+                    return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 }
