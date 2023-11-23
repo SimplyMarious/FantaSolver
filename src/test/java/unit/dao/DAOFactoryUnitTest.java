b0e37a20cbb7c88aeceff19ed476afe1f1efe491
rbgdotbrush <rbg94@hotmail.it>
2023-11-23 11:49:02 +0100

    Add unit tests on GetUserDAO

--- src/test/java/unit/dao/DAOFactoryUnitTest.java
+++ src/test/java/unit/dao/DAOFactoryUnitTest.java
@@ -3,21 +3,16 @@ package unit.dao;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.Mockito.*;
-
-import com.spme.fantasolver.dao.DAOFactory;
-import com.spme.fantasolver.dao.TeamDAO;
-import com.spme.fantasolver.dao.TeamDAOMySQL;
+import com.spme.fantasolver.dao.*;
 import com.spme.fantasolver.utility.Utility;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
-public class DAOFactoryUnitTest {
+class DAOFactoryUnitTest {
 
     @Test
-    public void testGetTeamDAOWithMySQLAsTeamDAOValueInProperties() {
+    void testGetTeamDAOWithMySQLAsTeamDAOValueInProperties() {
         try (MockedStatic<Utility> utilityMock = Mockito.mockStatic(Utility.class)) {
             utilityMock.when(() -> Utility.getValueFromProperties("teamDAO")).thenReturn("MySQL");
 
@@ -28,13 +23,35 @@ public class DAOFactoryUnitTest {
     }
 
     @Test
-    public void testGetTeamDAOWithAnyOtherStringAsTeamDAOValueInProperties() {
+    void testGetTeamDAOWithAnyOtherStringAsTeamDAOValueInProperties() {
         try (MockedStatic<Utility> utilityMock = Mockito.mockStatic(Utility.class)) {
             utilityMock.when(() -> Utility.getValueFromProperties("teamDAO")).thenReturn("SomeDBMS");
 
             TeamDAO result = DAOFactory.getTeamDAO();
 
             assertThat(result, is(instanceOf(TeamDAOMySQL.class)));
+        }
+    }
+
+    @Test
+    void testGetUserDAOWithMySQLAsUserDAOValueInProperties() {
+        try (MockedStatic<Utility> utilityMock = Mockito.mockStatic(Utility.class)) {
+            utilityMock.when(() -> Utility.getValueFromProperties("userDAO")).thenReturn("MySQL");
+
+            UserDAO result = DAOFactory.getUserDAO();
+
+            assertThat(result, is(instanceOf(UserDAOMySQL.class)));
+        }
+    }
+
+    @Test
+    void testGetUserDAOWithAnyOtherStringAsUserDAOValueInProperties() {
+        try (MockedStatic<Utility> utilityMock = Mockito.mockStatic(Utility.class)) {
+            utilityMock.when(() -> Utility.getValueFromProperties("userDAO")).thenReturn("SomeDBMS");
+
+            UserDAO result = DAOFactory.getUserDAO();
+
+            assertThat(result, is(instanceOf(UserDAOMySQL.class)));
         }
     }
 }
