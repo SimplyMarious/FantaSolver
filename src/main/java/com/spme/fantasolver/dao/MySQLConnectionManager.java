040681f32e55fd2a891789d82b5ef9a4e5cfb506
rbgdotbrush <rbg94@hotmail.it>
2023-11-29 15:32:37 +0100

    Update config.properties

--- src/main/java/com/spme/fantasolver/dao/MySQLConnectionManager.java
+++ src/main/java/com/spme/fantasolver/dao/MySQLConnectionManager.java
@@ -1,5 +1,8 @@
 package com.spme.fantasolver.dao;
 
+import com.spme.fantasolver.utility.Utility;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,10 +14,18 @@ public class MySQLConnectionManager {
 
     public static Connection connectToDatabase() throws ClassNotFoundException, SQLException{
         Connection connection;
+        String host;
+        String port;
+        try {
+            host = Utility.getValueFromProperties("host");
+            port = Utility.getValueFromProperties("port");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         Class.forName("com.mysql.cj.jdbc.Driver");
         connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/fantasolver?characterEncoding=utf8","root","root");
+                "jdbc:mysql://"+host+":"+port+"/fantasolver?characterEncoding=utf8","root","root");
         return connection;
     }
 }
