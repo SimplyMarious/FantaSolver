d89d6ae2248264bdd811bff4005c78eefe4d4cdd
rbgdotbrush <rbg94@hotmail.it>
2023-11-30 18:41:38 +0100

    Add MySQLConnectionManagerIntegrationTest

---
+++ src/test/java/integration/dao/MySQLConnectionManagerIntegrationTest.java
@@ -0,0 +1,30 @@
+package integration.dao;
+
+import com.spme.fantasolver.Main;
+import com.spme.fantasolver.utility.Utility;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static com.spme.fantasolver.dao.MySQLConnectionManager.connectToDatabase;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class MySQLConnectionManagerIntegrationTest {
+
+    @BeforeAll
+    public static void initialize() {
+        Utility.setPropertiesReadingTools(
+                new Properties(),
+                Main.class.getResourceAsStream("/config.properties"));
+    }
+
+    @Test
+    public void testConnectionToDatabase() throws SQLException, ClassNotFoundException {
+        Connection connection = connectToDatabase();
+        assertNotNull(connection);
+        connection.close();
+    }
+}
