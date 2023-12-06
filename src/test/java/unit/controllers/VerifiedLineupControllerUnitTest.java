d3c340f0cbcfdea2d16473b92dc3696050480f51
rbgdotbrush <rbg94@hotmail.it>
2023-12-06 18:58:59 +0100

    Add unit test for VerifiedLineupController

--- src/test/java/unit/controllers/VerifiedLineupControllerUnitTest.java
+++ src/test/java/unit/controllers/VerifiedLineupControllerUnitTest.java
@@ -2,7 +2,7 @@ package unit.controllers;
 
 import com.spme.fantasolver.controllers.FXMLLoadException;
 import com.spme.fantasolver.controllers.VerifiedLineupController;
-import com.spme.fantasolver.entity.Lineup;
+import com.spme.fantasolver.entity.*;
 import com.spme.fantasolver.ui.VerifiedLineupStage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,6 +39,30 @@ class VerifiedLineupControllerUnitTest {
         assertSame(verifiedLineupController, VerifiedLineupController.getInstance());
     }
 
+    @Test
+    void testHandleInitializationSuccess() throws IOException {
+        Lineup mockLineup = mock(Lineup.class);
+        Formation mockFormation = mock(Formation.class);
+        Player[] mockPlayers = new Player[1];
+        Slot[] mockSlots = new Slot[1];
+        mockPlayers[0] = mock(Player.class);
+        mockSlots[0] = mock(Slot.class);
+
+        doNothing().when(mockVerifiedLineupStage).initializeStage();
+        doNothing().when(mockVerifiedLineupStage).setLineupFormationLabelText(anyString());
+        doNothing().when(mockVerifiedLineupStage).loadPlayersInTable(mockPlayers);
+        doNothing().when(mockVerifiedLineupStage).show();
+        when(mockLineup.getFormation()).thenReturn(mockFormation);
+        when(mockFormation.getSlots()).thenReturn(mockSlots);
+        when(mockLineup.getPlayers()).thenReturn(mockPlayers);
+
+        verifiedLineupController.handleInitialization(mockLineup);
+
+        verify(mockVerifiedLineupStage, times(1)).initializeStage();
+        verify(mockVerifiedLineupStage, times(1)).setLineupFormationLabelText("Il tuo modulo: " + mockLineup.getFormation().getName());
+        verify(mockVerifiedLineupStage, times(1)).loadPlayersInTable(mockPlayers);
+        verify(mockVerifiedLineupStage, times(1)).show();
+    }
     @Test
     void testHandleInitializationWithFailure() throws IOException {
         doThrow(new IOException()).when(mockVerifiedLineupStage).initializeStage();
