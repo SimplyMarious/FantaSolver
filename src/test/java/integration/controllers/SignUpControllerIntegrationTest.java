16852d6edcebd851a954a08673939513d5123eb2
rbgdotbrush <rbg94@hotmail.it>
2023-12-07 17:14:53 +0100

    Add integration tests for SignUpController

---
+++ src/test/java/integration/controllers/SignUpControllerIntegrationTest.java
@@ -0,0 +1,71 @@
+package integration.controllers;
+
+import com.spme.fantasolver.Main;
+import com.spme.fantasolver.controllers.SignUpController;
+import com.spme.fantasolver.ui.*;
+import com.spme.fantasolver.utility.Utility;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static com.spme.fantasolver.dao.MySQLConnectionManager.connectToDatabase;
+import static org.mockito.Mockito.*;
+
+public class SignUpControllerIntegrationTest {
+    private SignUpController signUpController;
+    private SignUpStage mockSignUpStage;
+
+    @BeforeAll
+    static void initialize() {
+        Utility.setPropertiesReadingTools(
+                new Properties(),
+                Main.class.getResourceAsStream("/config.properties"));
+    }
+
+    @BeforeEach
+    void setup() {
+        mockSignUpStage = mock(SignUpStage.class);
+        signUpController = SignUpController.getInstance();
+        signUpController.setSignUpStage(mockSignUpStage);
+    }
+
+    private void removeUser(String username) throws ClassNotFoundException, SQLException {
+        Connection connection = connectToDatabase();
+        String deleteQuery = "DELETE FROM user WHERE name = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery);
+        preparedStatement.setString(1, username);
+        preparedStatement.executeUpdate();
+
+        connection.close();
+    }
+
+    @Test
+    void testHandlePressedSignUpButtonWithSuccessfulSignUp() throws SQLException, ClassNotFoundException {
+        String username = "testUser";
+        String password = "testPassword";
+
+        signUpController.handlePressedSignUpButton(username, password);
+
+        removeUser(username);
+        verify(mockSignUpStage).showSuccessfulSignUp();
+        verify(mockSignUpStage, never()).showFailedSignUp();
+    }
+
+    @Test
+    void testHandlePressedSignUpButtonWithFailureSignUp() throws SQLException, ClassNotFoundException {
+        String username = "testUser";
+        String password = "testPassword";
+        signUpController.handlePressedSignUpButton(username, password);
+
+        signUpController.handlePressedSignUpButton(username, password);
+
+        removeUser(username);
+        verify(mockSignUpStage).showFailedSignUp();
+    }
+
+}
